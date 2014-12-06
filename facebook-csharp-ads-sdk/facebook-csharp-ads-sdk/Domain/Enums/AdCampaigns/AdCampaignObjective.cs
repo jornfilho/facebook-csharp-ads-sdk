fa723e8dff4487dd207da3171bb38458bca5ae89
Michel Henrique Aquino Santos <michel.has@gmail.com>
2014-12-06 18:00:13 -0200

    Ad campaign model

---
+++ facebook-csharp-ads-sdk/facebook-csharp-ads-sdk/Domain/Enums/AdCampaigns/AdCampaignObjective.cs
@@ -0,0 +1,52 @@
+﻿using facebook_csharp_ads_sdk.Domain.Models.Attributes;
+
+namespace facebook_csharp_ads_sdk.Domain.Enums.AdCampaigns
+{
+    /// <summary>
+    ///     Objective of the ad campaign
+    /// </summary>
+    public enum AdCampaignObjective
+    {
+        [FacebookName("")]
+        Undefined = 0,
+
+        [FacebookName("NONE")]
+        None = 1,
+
+        [FacebookName("OFFER_CLAIMS")]
+        OfferClaims = 2,
+
+        [FacebookName("PAGE_LIKES")]
+        PageLikes = 3,
+
+        [FacebookName("CANVAS_APP_INSTALLS")]
+        CanvasAppInstalls = 4,
+
+        [FacebookName("CANVAS_APP_ENGAGEMENT")]
+        CanvasAppEngagement = 5,
+
+        [FacebookName("EVENT_RESPONSES")]
+        EventResponses = 6,
+
+        [FacebookName("LOCAL_AWARENESS")]
+        LocalAwareness = 7,
+
+        [FacebookName("POST_ENGAGEMENT")]
+        PostEngagement = 8,
+
+        [FacebookName("WEBSITE_CONVERSIONS")]
+        WebsiteConversions = 9,
+
+        [FacebookName("MOBILE_APP_INSTALLS")]
+        MobileAppInstalls = 10,
+
+        [FacebookName("MOBILE_APP_ENGAGEMENT")]
+        MobileAppEngagement = 11,
+
+        [FacebookName("WEBSITE_CLICKS")]
+        WebsiteClicks = 12,
+
+        [FacebookName("VIDEO_VIEWS")]
+        VideoViews = 13
+    }
+}
---
+++ facebook-csharp-ads-sdk/facebook-csharp-ads-sdk/Domain/Models/AdCampaigns/AdCampaign.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using facebook_csharp_ads_sdk.Domain.Enums.AdCampaigns;
+
+namespace facebook_csharp_ads_sdk.Domain.Models.AdCampaigns
+{
+    public class AdCampaign : BaseCrudObject<AdCampaign>
+    {
+        public long Id { get; private set; }
+
+        public long AccountId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public AdCampaignObjective Objective { get; private set; }
+
+        // TODO: criar enum
+        public string Status { get; private set; }
+
+        // TODO: criar enum
+        public string BuyingType { get; private set; }
+
+        public override AdCampaign ReadSingle(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AdCampaign ParseSingleResponse(string response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
