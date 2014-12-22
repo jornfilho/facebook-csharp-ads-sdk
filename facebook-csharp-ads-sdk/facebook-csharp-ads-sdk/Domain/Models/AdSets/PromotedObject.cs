7ef4cbec663fa87991e1d91ebf56bc638ebc456c
Michel Henrique Aquino Santos <michel.has@gmail.com>
2014-12-22 17:07:59 -0200

    Promoted object set data to create method

--- facebook-csharp-ads-sdk/facebook-csharp-ads-sdk/Domain/Models/AdSets/PromotedObject.cs
+++ facebook-csharp-ads-sdk/facebook-csharp-ads-sdk/Domain/Models/AdSets/PromotedObject.cs
@@ -1,4 +1,7 @@
-﻿using facebook_csharp_ads_sdk.Domain.Enums.Configurations;
+﻿using System;
+using facebook_csharp_ads_sdk.Domain.Enums.AdCampaigns;
+using facebook_csharp_ads_sdk.Domain.Enums.Configurations;
+using facebook_csharp_ads_sdk.Domain.Exceptions.AdSet;
 using facebook_csharp_ads_sdk.Domain.Models.Attributes;
 
 namespace facebook_csharp_ads_sdk.Domain.Models.AdSets
@@ -42,5 +45,56 @@ namespace facebook_csharp_ads_sdk.Domain.Models.AdSets
         [FacebookName("object_store_url")]
         [FacebookFieldTypeAttribute(FacebookFieldType.String)]
         public string ObjectStoreUrl { get; private set; }
+
+        /// <summary>
+        ///     Set data to create on Facebook
+        /// </summary>
+        /// <param name="adCampaignObjective"> Ad campaign objective </param>
+        /// <param name="objectId"> Object id, according to the objective </param>
+        /// <param name="objectStoreUrl"> Object </param>
+        /// <exception cref="InvalidObjectIdException"> Invalid object id </exception>
+        /// <exception cref="InvalidObjectStoreUrlException"> Invalid object store url </exception>
+        /// <returns> This instance </returns>
+        public PromotedObject SetDataToCreate(AdCampaignObjectiveEnum adCampaignObjective, long objectId, string objectStoreUrl)
+        {
+            if (objectId <= 0)
+            {
+                throw new InvalidObjectIdException();
+            }
+
+            switch (adCampaignObjective)
+            {
+                case AdCampaignObjectiveEnum.WebsiteConversions:
+                    this.PixelId = objectId;
+                    break;
+                case AdCampaignObjectiveEnum.PageLikes:
+                    this.PageId = objectId;
+                    break;
+                case AdCampaignObjectiveEnum.OfferClaims:
+                    this.OfferId = objectId;
+                    break;
+                case AdCampaignObjectiveEnum.MobileAppInstalls:
+                case AdCampaignObjectiveEnum.MobileAppEngagement:
+                case AdCampaignObjectiveEnum.CanvasAppInstalls:
+                case AdCampaignObjectiveEnum.CanvasAppEngagement:
+                    if (String.IsNullOrEmpty(objectStoreUrl))
+                    {
+                        throw new InvalidObjectStoreUrlException();
+                    }
+
+                    this.ApplicationId = objectId;
+                    this.ObjectStoreUrl = objectStoreUrl;
+                    break;
+                default:
+                    this.PixelId = null;
+                    this.PageId = null;
+                    this.OfferId = null;
+                    this.ApplicationId = null;
+                    this.ObjectStoreUrl = null;
+                    break;
+            }
+
+            return this;
+        }
     }
 }
